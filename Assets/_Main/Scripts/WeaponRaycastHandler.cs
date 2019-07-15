9b51a3bc034466d702f110eecce1f17d512d2f68
PeterEldredge <peldredge98@gmail.com>
2019-07-15 17:56:17 -0400

    Tweaks

--- Assets/_Main/Scripts/WeaponRaycastHandler.cs
+++ Assets/_Main/Scripts/WeaponRaycastHandler.cs
@@ -29,11 +29,18 @@ public class WeaponRaycastHandler : GameEventUserObject
     private void FireWeapon(WeaponFiredEventArgs eventArgs)
     {
         RaycastHit hit;
-        if(Physics.Raycast(_camera.transform.position, _camera.transform.forward, out hit, _range, LayerMask.GetMask("MoveableObject")))
+        if(Physics.Raycast(_camera.transform.position, _camera.transform.forward, out hit, _range))
         {
-            MoveableBlock block = hit.transform.GetComponentInParent<MoveableBlock>();
-            if(block) block.HandleHit(new HitData(eventArgs.FireTypeArg, hit));
-            else Debug.LogError("No MoveableBlock component found!");
-        }   
+            if(hit.transform.gameObject.layer == LayerMask.NameToLayer("MoveableObject"))
+            {
+                MoveableBlock block = hit.transform.GetComponentInParent<MoveableBlock>();
+                if(block) block.HandleHit(new HitData(eventArgs.FireTypeArg, hit));
+                else Debug.LogError("No MoveableBlock component found!");
+            }
+            else
+            {
+                AudioManager.Instance.PlayDudSound();
+            } 
+        } 
     }
 }
