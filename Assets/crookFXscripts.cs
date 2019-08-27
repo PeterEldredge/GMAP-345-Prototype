367073c10c05e3d8ec4fe97dc5615de452e13f79
glangenhorst <52493121+glangenhorst@users.noreply.github.com>
2019-08-27 12:01:41 -0400

    Added crook + particleFX

---
+++ Assets/crookFXscripts.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crookFXscripts : MonoBehaviour
+
+{
+    public ParticleSystem blueFlash;
+    public ParticleSystem redFlash;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            blueFlash.Play();
+
+
+        if (Input.GetMouseButtonDown(1))
+            redFlash.Play();
+
+    }
+}
